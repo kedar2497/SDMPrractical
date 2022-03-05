b90ba3d7a77e4af23a6d8ebd0a0e9988a89b97e5
kedar kumbhar <kedarkumbhar27@gmail.com>
2022-03-05 11:07:36 +0530

    FirstProgram

---
+++ FirstProgram/src/Com/app/kk/Program.java
@@ -0,0 +1,12 @@
+package Com.app.kk;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
